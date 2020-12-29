1ef76e7a8b863168ab3b84445c0f50897a0c403e
Jeevan522 <jeevan0522@gmail.com>
2020-12-29 09:33:00 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform{
+	required_version = "=0.12.3"
+	backend "azurerm" {
+	   resource_group_name = "demo"
+	   key = "terraform.tfstate"
+	}
+}
+
+provider "azurerm"{
+  version = "~>2.0"
+  features {}
+  
+}
+
+resource "azurerm_resource_group" "rg-name" {
+  name = "Hello-Word-ResourceGroup"
+  location = "Central-US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name = "Hello-World-AKS"
+  location = "Central-US"
+  resource_group_name = rg-name.name
+  kubernetes_version = "1.18.8"
+  node_resource_group = "Node-RG"
+  
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
