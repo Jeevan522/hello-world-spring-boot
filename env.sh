86655b43ae97c978c7f141649a03880114f6ae17
Jeevan522 <jeevan0522@gmail.com>
2020-12-29 10:33:46 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,7 +4,14 @@ echo "Terraform Initializing...."
 
 terraform version
 
-terraform init 
+terraform init \
+    -backend-config=storage_account_name\=demo10 \
+    -backend-config=resource_group_name\=demo \
+    -backend-config=key\=terraform.tfstate \
+    -backend-config=container_name\=devops\
+    -get=true \
+    -get-plugins=true \
+    -verify-plugins=true
     
 	
 echo "completed"
